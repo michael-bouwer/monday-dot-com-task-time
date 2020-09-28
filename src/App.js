17d5c73a170944d8caf857222a058434f3c7172e
Michael Bouwer <mbouwer3@gmail.com>
2020-09-28 21:07:29 +0200

    Configured Apollo Client with Monday API token. Successfully connects to the API.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
 import "./App.css";
+import { gql } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import mondaySdk from "monday-sdk-js";
 const monday = mondaySdk();
+
+const httpLink = createHttpLink({
+  uri: 'https://api.monday.com/v2/',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjU4NzkyNDMyLCJ1aWQiOjE0OTcwODg2LCJpYWQiOiIyMDIwLTA3LTExVDE3OjUxOjA2LjAwMFoiLCJwZXIiOiJtZTp3cml0ZSJ9.uC-owvux2QA0OdtWec5QcxxMNMFNtDWPDtHsyRkz3DQ';
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 class App extends React.Component {
   constructor(props) {
@@ -12,6 +36,18 @@ class App extends React.Component {
       settings: {},
       name: "",
     };
+
+    client
+      .query({
+        query: gql`
+          query {
+            boards(ids: 642236546) {
+              name
+            }
+          }
+        `,
+      })
+      .then((result) => console.log(result));
   }
 
   componentDidMount() {
@@ -19,7 +55,7 @@ class App extends React.Component {
   }
 
   render() {
-    return <div className="App">Hello, monday Apps!</div>;
+    return <div className="App primary">Hello, monday Apps!</div>;
   }
 }
 
