fc3f638577fd711b50ff156696a79ea722bb93da
Michael Bouwer <mbouwer3@gmail.com>
2020-11-03 21:41:52 +0200

    Removed experimental header reference passing

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,16 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { ReactComponent as LoadingSVG } from "../../assets/svg/loading.svg";
 import "./styles.scss";
 
-function Loading({ text, heightDiff }) {
-  useEffect(() => {
-    console.log(heightDiff);
-  }, []);
+function Loading({ text }) {
   return (
-    <div
-      className="loading"
-      style={{ position: "absolute", left: "unset", top: "unset" }}
-    >
+    <div className="loading" style={{position: "absolute", left: "unset", top: "unset"}}>
       <div className="modal">
         <LoadingSVG width="100px" height="100px" />
         <div>
